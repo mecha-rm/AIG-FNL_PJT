507d082d565371ff60ba960a823f193eca796d53
Michael Law <michael.law@ontariotechu.net>
2022-03-27 02:02:52 -0400

    Update ComputerPlayer.cs

--- Assets/Scripts/ComputerPlayer.cs
+++ Assets/Scripts/ComputerPlayer.cs
@@ -5,11 +5,20 @@ using UnityEngine;
 // Q-value storing object
 public class QValueStore
 {
-    // state, speed, angle, actions (speed up, speed down, turn left, turn right)
-    float[,,,] array = new float[4,4,4,4];
-    public float getQValue(state, action);
-    public int getBestAction(state);
-    public void storeQValue(state, action, float value);
+    // state, actions
+    float[,] array = new float[4,4];
+    public float getQValue(int state, int action)
+    {
+        return 0f;
+    }
+    public int getBestAction(int state)
+    {
+        return 0;
+    }
+    public void storeQValue(int state, int action, float value)
+    {
+
+    }
 }
 
 public class ReinforcementProblem
@@ -25,11 +34,11 @@ public class ReinforcementProblem
     //  function takeAction(state, action)
 }
 
-public QValueStore store = new QValueStore()
-
 // this is the script for the player that's controlled by the computer.
 public class ComputerPlayer : Player
 {
+    public QValueStore store = new QValueStore();
+
     // Start is called before the first frame update
     new void Start()
     {
@@ -43,47 +52,49 @@ public class ComputerPlayer : Player
     }
 
     // Updates the store by investigating the problem.
-    public void QLearning(problem, int iterations, alpha, gamma, rho, nu)
-    {
-        // Get a starting state.
-        state = problem.getRandomState()
+    //I am assuming that the random function returns a floating point number between zero and
+    //one.The oneOf function picks an item from a list at random.
+    //public void QLearning(problem, int iterations, alpha, gamma, rho, nu)
+    //{
+    //    // Get a starting state.
+    //    state = problem.getRandomState()
 
-        // Repeat a number of times.
-        for (int x = 0; x < iterations; x++)
-        {
-            // Pick a new state every once in a while.
-            if (random() < nu)
-                state = problem.getRandomState();
+    //    // Repeat a number of times.
+    //    for (int x = 0; x < iterations; x++)
+    //    {
+    //        // Pick a new state every once in a while.
+    //        if (random() < nu)
+    //            state = problem.getRandomState();
 
-            // Get the list of available actions.
-            actions = problem.getAvailableActions(state);
+    //        // Get the list of available actions.
+    //        actions = problem.getAvailableActions(state);
 
-            // Should we use a random action this time?
-            if (random() < rho) {
-                action = oneOf(actions);
-            } else {
-                // Otherwise pick the best action.
-                action = store.getBestAction(state)
-            }
-            // Carry out the action and retrieve the reward and new state.
-            reward, newState = problem.takeActions(state, action)
+    //        // Should we use a random action this time?
+    //        if (random() < rho) {
+    //            action = oneOf(actions);
+    //        } else {
+    //            // Otherwise pick the best action.
+    //            action = store.getBestAction(state)
+    //        }
+    //        // Carry out the action and retrieve the reward and new state.
+    //        reward, newState = problem.takeActions(state, action)
 
-            // Get the current q from the store.
-            Q = store.getQValue(state, action);
+    //        // Get the current q from the store.
+    //        Q = store.getQValue(state, action);
 
-            // Get the q of the best action from the new state.
-            maxQ = store.getQValue(newState, store.getBestAction(newState));
+    //        // Get the q of the best action from the new state.
+    //        maxQ = store.getQValue(newState, store.getBestAction(newState));
 
-            // Perform the q learning.
-            Q = (1 - alpha) * Q + alpha * (reward + gamma * maxQ);
+    //        // Perform the q learning.
+    //        Q = (1 - alpha) * Q + alpha * (reward + gamma * maxQ);
 
-            // Store the new Q-value.
-            store.storeQValue(state, action, Q);
+    //        // Store the new Q-value.
+    //        store.storeQValue(state, action, Q);
 
-            // And update the state.
-            state = newState;
-        }
-    }
+    //        // And update the state.
+    //        state = newState;
+    //    }
+    //}
 }
 
 
